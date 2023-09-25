74c3e5bedcdcb4d3a98f5755760e768981be5466
Женя <kilnod324@gmail.com>
2023-09-25 18:23:33 +0300

    num

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 a = int(input())
 b = int(input())
-c = int(input())
-d = int(input())
-e = int(input())
-print((a*b*c)+(d/e))
+S = a + b
+print(S)
---
+++ num.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+c = int(input())
+e = int(input())
+d = int(input())
+print((a*b*c)+(d/e))
+
